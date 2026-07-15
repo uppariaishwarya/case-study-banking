ba816149ee6bf7914633f3491699e914bf2b4f38
uppari-aishwarya <uppari.aishwarya@capgemini.com>
2026-07-15 14:34:00 +0530

    update

--- api-gateway/src/main/java/com/bank/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bank/apigateway/ApiGatewayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
